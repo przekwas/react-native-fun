66ed41143fb083a35a811df224ad0e2841a9fd96
Luke Przekwas <galactica@The-Galactica.local>
2018-05-15 13:08:13 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# React Native Fun!
+
+## Making my own little react native app as a learning sandbox
+One of my primary goals was to get a better handle on mastering react native flexbox styling and making my own components without using a UI toolkit!
+
+I made my own API to show the albums and their titles so I can map my own cards out in the app
+
+### Running
+I used the react-native-cli to generate the react native app, so running it will require you have that
+
+Clone the repo, npm install, and react-native run-ios !
---
+++ src/components/AlbumList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            albums: []
+        }
+    }
+
+    componentWillMount() {
+        fetch('https://lukes-projects.herokuapp.com/api/music_albums')
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({ albums: responseData });
+            });
+    }
+
+    renderAlbums() {
+        return this.state.albums.map((album) => 
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
+    render() {
+
+        return (
+            <ScrollView>
+                {this.renderAlbums()}
+            </ScrollView>
+        );
+    }
+
+};
+
+export default AlbumList;
